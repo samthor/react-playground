40c8e7721a26c29fd4a7de8909f87be009915cc6
Sam Thorogood <sam@localhost>
2021-12-16 10:04:22 +1100

    playground

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Form() {
+  return (
+    <form>
+      <h1>Report Card App</h1>
+    </form>
+  )
+}
+
+ReactDOM.render(<Form />, document.getElementById('root'));
